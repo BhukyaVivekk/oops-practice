fb505aa586e6b29b7e06e8c88ec1d9984e8e1134
vivek1 <bhukyavivek007@gmail.com>
2025-01-06 12:59:11 +0530

    updated inheritance

--- src/emphierarchy/Employee.java
+++ src/emphierarchy/Employee.java
@@ -13,7 +13,7 @@ public class Employee {
     }
 
     public void printDetails(){
-        System.out.println("Employee Details : ");
+        System.out.println("Employee Profile : "+this.getClass().getSimpleName());
         System.out.printf("""
                 ID : %d
                 Name : %s
